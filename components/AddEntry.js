cc29ff569d78cb7f7b9aa1de9cf1737bf34ff3e9
Newville23 <newville21@gmail.com>
2018-01-05 10:02:55 -0500

    feat: Add AddEntry Component & DateHeader

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,12 +1,72 @@
-import React , { Component } from 'react'
-import {View, Text} from 'react-native'
+import React, { Component } from 'react'
+import {View} from 'react-native'
 import { getMetricMetaInfo } from '../utils/helpers'
+import UdaciSlider from './UdaciSlide';
+import UdaciSteper from './UdaciSteper';
+import DateHeader from './DateHeader';
 
 export default class AddEntry extends Component{
+    state = {
+        run: 0,
+        bike: 0,
+        swim: 0,
+        sleep: 0,
+        eat: 0,
+    }
+    increment = (metric) => {
+        const {max, step} = getMetricMetaInfo(metric)
+
+        this.setState((state)=>{
+            const count = state[metric] + step
+            return {
+                ...state,
+                [metric]: count > max ? max : count, 
+            }
+        })
+    }
+    decrement = (metric) => {
+        this.setState((state) => {
+            const count = state[metric] - getMetricMetaInfo(metric).step
+            return {
+                ...state,
+                [metric]: count < 0 ? 0 : count, 
+            }
+        })
+    }
+    slide = (metric, value) => {
+        this.setState(() => ({
+            [metric]: value,
+        }))
+    }
     render() {
-        return (
+        const metaInfo = getMetricMetaInfo()
+        return(
             <View>
-               {getMetricMetaInfo('bike')}
+                <DateHeader date={(new Date()).toLocaleDateString()}/>
+               {Object.keys(metaInfo).map((key) => {
+                   const { getIcon, type, ...rest} = metaInfo[key]
+                   const value = this.state[key]
+                    return(
+                       <View key={key}>
+                        {getIcon()}
+                        {type === 'slider'
+                            ? <UdaciSlider
+                                value={value}
+                                onChange={() => this.slide(key, value)}
+                                {...rest}
+                                />  
+                            : <UdaciSteper
+                                value={value}
+                                onIncrement={() => this.increment()}
+                                onDecrement={() => this.decrement()}
+                                {...rest}
+                                />
+                        }
+                       </View> 
+                    )
+               })
+                    
+               }
             </View>
         )
     }
---
+++ components/UdaciSteper.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import { View, Text } from 'react-native'
+
+export default function UdaciSteper() {
+    return (
+        <View>
+            <Text>
+                UdaciSteper
+            </Text>
+        </View>
+    )
+}
